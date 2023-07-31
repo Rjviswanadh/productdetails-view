edfa585195450be2e47ab5162b359051a6fa9a2b
Rjviswanadh <96373360+Rjviswanadh@users.noreply.github.com>
2023-07-31 17:38:21 +0530

    Update index.js

--- src/components/ProductItemDetails/index.js
+++ src/components/ProductItemDetails/index.js
@@ -1,3 +1,4 @@
+import {BsPlusSquare, BsDashSquare} from 'react-icons/bs'
 import Loader from 'react-loader-spinner'
 import {Link} from 'react-router-dom'
 import {Component} from 'react'
@@ -18,7 +19,7 @@ class ProductItemDetails extends Component {
     displayList: [],
     count: 1,
     apiStatus: apiStatusConstants.initial,
-    isPresent: false,
+    error: '',
   }
 
   componentDidMount() {
@@ -27,13 +28,13 @@ class ProductItemDetails extends Component {
 
   getById = async () => {
     this.setState({apiStatus: apiStatusConstants.inProgress})
-    const {history} = this.props
-    const {location} = history
-    const {pathname} = location
-    console.log(pathname)
+    const {match} = this.props
+    const {params} = match
+    const {id} = params
+    //  console.log(pathname)
     const jwtToken = Cookies.get('jwt_token')
 
-    const url = `https://apis.ccbp.in${pathname}`
+    const url = `https://apis.ccbp.in/products/${id}`
 
     const option = {
       headers: {
@@ -43,6 +44,7 @@ class ProductItemDetails extends Component {
     }
     const response = await fetch(url, option)
     const data = await response.json()
+
     const list = {
       availability: data.availability,
       brand: data.brand,
@@ -55,23 +57,53 @@ class ProductItemDetails extends Component {
     this.setState({
       displayList: list,
       apiStatus: apiStatusConstants.success,
-      isPresent: true,
+      // isPresent: true,
     })
     if (data.status_code === 404) {
-      this.setState(pv => ({isPresent: !pv.isPresent}))
+      console.log(data.error_msg)
+      // this.setState(pv => ({isPresent: !pv.isPresent}))
+      this.setState(() => ({error: data.error_msg}))
     }
   }
 
   clickToDelete = () => {
-    this.setState(pv => ({count: pv.count - 1}))
+    const {count} = this.state
+    if (count === 1) {
+      this.setState(() => ({count: 1}))
+    } else {
+      this.setState(pv => ({count: pv.count - 1}))
+    }
   }
 
   clickToAdd = () => {
     this.setState(pv => ({count: pv.count + 1}))
   }
 
+  itemNotFound = () => {
+    const {error} = this.state
+
+    return (
+      <>
+        <div className="item-not-found">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
+            className="no-product-img"
+            alt="error view"
+          />
+          <h1>{error}</h1>
+          <Link to="/products">
+            <button className="shopping" type="button">
+              Continue Shopping
+            </button>
+          </Link>
+        </div>
+      </>
+    )
+  }
+
   productItemsDetails = () => {
-    const {displayList, count, isPresent} = this.state
+    const {displayList, count} = this.state
+    console.log(displayList)
     const {
       imageUrl,
       price,
@@ -80,24 +112,25 @@ class ProductItemDetails extends Component {
       description,
       availability,
     } = displayList
-
     return (
       <>
         <Header />
         <div>
-          {isPresent ? (
+          {displayList.similarProducts !== undefined ? (
             <div>
               <div className="display-size">
                 <img src={imageUrl} alt="product" className="display-image" />
                 <div>
                   <h1>{brand}</h1>
                   <p>RS {price} /- </p>
-                  <div className="bg-color">
-                    <img
-                      src="https://assets.ccbp.in/frontend/react-js/star-img.png"
-                      alt="star"
-                    />
-                    <p>{rating}</p>
+                  <div>
+                    <div className="bg-color">
+                      <img
+                        src="https://assets.ccbp.in/frontend/react-js/star-img.png"
+                        alt="star"
+                      />
+                      <p>{rating}</p>
+                    </div>
                   </div>
                   <p>{description}</p>
                   <p>
@@ -114,7 +147,7 @@ class ProductItemDetails extends Component {
                       className="button1"
                       testid="minus"
                     >
-                      -
+                      <BsDashSquare />
                     </button>
                     <p>{count}</p>
                     <button
@@ -123,36 +156,28 @@ class ProductItemDetails extends Component {
                       onClick={this.clickToAdd}
                       testid="plus"
                     >
-                      +
+                      <BsPlusSquare />
                     </button>
                   </div>
-                  <button type="button">ADD TO CART</button>
+                  <button type="button" className="similar-button">
+                    ADD TO CART
+                  </button>
                 </div>
               </div>
-              <ul>
-                <p>Similar Products</p>(
-                <div className="similar-product">
+              <div>
+                <h1 className="similar-h1">Similar Products</h1>
+                <ul className="similar-product">
                   {displayList.similarProducts.map(eachItem => (
                     <SimilarProductItem
                       similarOne={eachItem}
                       key={eachItem.id}
                     />
                   ))}
-                </div>
-              </ul>
+                </ul>
+              </div>
             </div>
           ) : (
-            <>
-              <img
-                src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-error-view-img.png"
-                className="no-product-img"
-                alt="error view"
-              />
-              <h1>Product Not Found</h1>
-              <Link to="/products">
-                <button type="button">Continue Shopping</button>
-              </Link>
-            </>
+            <div>{this.itemNotFound()}</div>
           )}
         </div>
       </>
